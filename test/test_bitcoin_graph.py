aa4ccecde7f2599f3576ff717e98b177fdd098c7
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-09-28 12:04:06 -0400

    test - updated test methods

--- test/test_bitcoin_graph.py
+++ test/test_bitcoin_graph.py
@@ -3,7 +3,30 @@ import unittest
 
 class TestBitcoinGraph:
 
-    pass
+    def test_timestamp_conv():
+        pass
+
+    def test_bitcoin_data():
+        pass
+
+    def test_bitcoin_network():
+        """
+
+        """
+        pass
+
+    def test_bitcoin_graph():
+        """
+
+        """
+        pass
+
+    def test_bitcoin_data_export():
+        """
+        Need to make sure the exceptions are set each export with
+        corresponding export error
+        """
+        pass
 
 
 if __name__ == "__main__":
