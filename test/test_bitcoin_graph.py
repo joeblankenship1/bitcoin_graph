df8349664b0ad17b242153fae00fdfe25556cb96
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-10-07 21:32:08 -0400

    test - added bitcoin_network method

--- test/test_bitcoin_graph.py
+++ test/test_bitcoin_graph.py
@@ -18,10 +18,17 @@ class TestBitcoinGraph(unittest.TestCase):
         self.assertIn(bitcoin_data(data)[0][2]["time"], '2019-10-07 06:27:39', "Should contain 2019-10-07 06:27:39")
         self.assertIn(bitcoin_data(data)[0][2]["relay"], '127.0.0.1', "Should contain 127.0.0.1")
 
-    '''
-    def test_bitcoin_network():
-        pass
+    def test_bitcoin_network(self):
+        data = [('1DqeUNa3wqJRamTEUMTiUXHAQynQuLh426', {'name': 'Near Genesis'})]
+        self.assertIn(bitcoin_network(data)[0][0], '1DqeUNa3wqJRamTEUMTiUXHAQynQuLh426', "Should contain 1DqeUNa3wqJRamTEUMTiUXHAQynQuLh426")
+        self.assertIn(bitcoin_network(data)[0][1], '1NsooQ8fvpJinqyp3hDtSBUxKMQFUxXEkE', "Should contain 1NsooQ8fvpJinqyp3hDtSBUxKMQFUxXEkE")
+        self.assertEqual(bitcoin_network(data)[0][2]["input_value"], 21383, "Should contain 21383")
+        self.assertEqual(bitcoin_network(data)[0][2]["output_value"], 7928, "Should contain 7928")
+        self.assertIn(bitcoin_network(data)[0][2]["hash"], '2fe15db1f629e873aa7b8ef1e5adc7c323667a6f1eb83cfb5b6cbd04c1329df8', "Should contain 2fe15db1f629e873aa7b8ef1e5adc7c323667a6f1eb83cfb5b6cbd04c1329df8")
+        self.assertIn(bitcoin_network(data)[0][2]["time"], '2019-10-07 06:27:39', "Should contain 2019-10-07 06:27:39")
+        self.assertIn(bitcoin_network(data)[0][2]["relay"], '127.0.0.1', "Should contain 127.0.0.1")
 
+    '''
     def test_bitcoin_graph():
         pass
 
